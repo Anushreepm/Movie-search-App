b197dbed04736e5624cea58e0ffcad97633e33e3
Anushree P M <65195869+Anushreepm@users.noreply.github.com>
2020-09-24 20:47:33 +0530

    Updates

---
+++ precache-manifest.82e882901c1b232e2f01932209b1e66a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f47fa87faa02c708d0a1ad5cd16d9162",
+    "url": "/Movie-search-app/index.html"
+  },
+  {
+    "revision": "eddd417b50489230a134",
+    "url": "/Movie-search-app/static/css/main.4059e150.chunk.css"
+  },
+  {
+    "revision": "e3f2c81dfa9e1efd6c24",
+    "url": "/Movie-search-app/static/js/2.94f16e27.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Movie-search-app/static/js/2.94f16e27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eddd417b50489230a134",
+    "url": "/Movie-search-app/static/js/main.ec01b836.chunk.js"
+  },
+  {
+    "revision": "daba2869a9142c9083b4",
+    "url": "/Movie-search-app/static/js/runtime-main.06230eb6.js"
+  }
+]);
